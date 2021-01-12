089778003197a05490191d34668dc8bd297a5884
Felix Fontein <felix@fontein.de>
2021-01-12 07:30:31 +0100

    Improve comment.

--- tests/integration/targets/lookup_hashi_vault/runme.sh
+++ tests/integration/targets/lookup_hashi_vault/runme.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-export ANSIBLE_TEST_PREFER_VENV=1  # see https://github.com/ansible/ansible/pull/73000#issuecomment-757012395
+export ANSIBLE_TEST_PREFER_VENV=1  # see https://github.com/ansible/ansible/pull/73000#issuecomment-757012395; can be removed once Ansible 2.9 and ansible-base 2.10 support has been dropped
 source virtualenv.sh
 
 # First install pyOpenSSL, then test lookup in a second playbook in order to
