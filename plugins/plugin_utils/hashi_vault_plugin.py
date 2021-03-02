6951b0ff9246d4b19740203b0f83afd427e504bb
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-03-02 08:14:37 -0500

    Add internal use disclaimer to plugin_util

--- plugins/plugin_utils/hashi_vault_plugin.py
+++ plugins/plugin_utils/hashi_vault_plugin.py
@@ -2,6 +2,11 @@
 # Copyright (c) 2021 Brian Scholer (@briantist)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+# FOR INTERNAL COLLECTION USE ONLY
+# The interfaces in this file are meant for use within the community.hashi_vault collection
+# and may not remain stable to outside uses. Changes may be made in ANY release, even a bugfix release.
+# See also: https://github.com/ansible/community/issues/539#issuecomment-780839686
+# Please open an issue if you have questions about this.
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
