e17340f4f3d2af851287911f71fda31f83970f0a
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-04-12 16:55:02 -0400

    test shuffle

--- tests/integration/targets/_lookup_hashi_vault/runme.sh
+++ tests/integration/targets/_lookup_hashi_vault/runme.sh
@@ -5,6 +5,17 @@ set -eux
 export ANSIBLE_TEST_PREFER_VENV=1  # see https://github.com/ansible/ansible/pull/73000#issuecomment-757012395; can be removed once Ansible 2.9 and ansible-base 2.10 support has been dropped
 source virtualenv.sh
 
+# (thanks https://github.com/ansible-collections/servicenow.itsm/blob/main/tests/integration/targets/inventory/runme.sh)
+# When running script-based integration targets, `ansible-test integration`
+# does not make these variables available. We will need to pass them
+# explicitly, as extra vars to `ansible-playbook` command.
+readonly vars_file=../../integration_config.yml
+
+if [[ ! -f "$vars_file" ]]; then
+    # cp -T "${vabs}.template" "$vabs"
+    echo -e "---\ndummy: 0" > "$vars_file"
+fi
+
 # First install pyOpenSSL, then test lookup in a second playbook in order to
 # workaround this error which occurs on OS X 10.11 only:
 #
@@ -20,7 +31,7 @@ source virtualenv.sh
 #   fatal: [testhost]: FAILED! => { "msg": "An unhandled exception occurred while running the lookup plugin 'hashi_vault'. Error was a <class 'requests.exceptions.SSLError'>, original message: HTTPSConnectionPool(host='localhost', port=8201): Max retries exceeded with url: /v1/auth/token/lookup-self (Caused by SSLError(SSLError(\"bad handshake: Error([('SSL routines', 'ssl3_get_server_certificate', 'certificate verify failed')],)\",),))"}
 
 ANSIBLE_ROLES_PATH=../ \
-    ansible-playbook playbooks/install_dependencies.yml -v "$@"
+    ansible-playbook playbooks/install_dependencies.yml -e "@$vars_file" -v "$@"
 
 ANSIBLE_ROLES_PATH=../ \
-    ansible-playbook playbooks/test_lookup_hashi_vault.yml -v "$@"
+    ansible-playbook playbooks/test_lookup_hashi_vault.yml -e "@$vars_file" -v "$@"
