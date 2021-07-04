2100101c1944cae44417e4e75652a2376a053fe0
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-07-03 22:29:00 -0400

    -ezenvA B C

--- .github/actions/easyenv/easyenv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-while IFS= read -r line ; do
-    if [[ "$line" =~ [^[:space:]] ]] ; then
-        eval "$line"
-        __var=${line%%=*}
-        echo "${__var}=${!__var}" >> "${GITHUB_ENV}"
-    fi
-done
