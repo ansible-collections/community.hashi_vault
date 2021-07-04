6658fc5f2a1a4d19ec6d52a35d384556b5ec8ffa
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-07-03 20:18:42 -0400

    trying

--- .github/actions/easyenv/easyenv.sh
+++ .github/actions/easyenv/easyenv.sh
@@ -2,5 +2,8 @@
 while IFS= read -r line ; do
     eval "$line"
     __var=${line%%=*}
+    echo "$line"
+    echo "${__var}"
+    echo "${!__var}"
     echo "${__var}=${!__var}" >> "${GITHUB_ENV}"
 done
