3ce6825829b5c15750491cfa7ce224af934491f1
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-07-08 00:04:38 -0400

    localenv workflows

--- tests/integration/targets/setup_localenv_docker/setup.sh
+++ tests/integration/targets/setup_localenv_docker/setup.sh
@@ -5,4 +5,7 @@ pushd "${BASH_SOURCE%/*}/files"
 ANSIBLE_ROLES_PATH="../../" \
     ansible-playbook playbooks/vault_docker.yml "${@}"
 
+# copy generated integration_config.yml if it doesn't exist
+cp -n .output/integration_config.yml ../../../ || true
+
 popd
--- tests/integration/targets/setup_localenv_gha/setup.sh
+++ tests/integration/targets/setup_localenv_gha/setup.sh
@@ -5,6 +5,10 @@ pushd "${BASH_SOURCE%/*}/files"
 ANSIBLE_ROLES_PATH=../../ \
     ansible-playbook playbooks/gha.yml "${@}"
 
+# launch containers
 .output/launch.sh
+
+# copy generated integration_config.yml if it doesn't exist
+cp -n .output/integration_config.yml ../../../ || true
 
 popd
