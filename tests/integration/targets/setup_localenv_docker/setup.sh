a1a8ede1cde076113e9b2c07e38fc2f777c94672
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-07-08 14:31:09 -0400

    files!files!files!

--- tests/integration/targets/setup_localenv_docker/setup.sh
+++ tests/integration/targets/setup_localenv_docker/setup.sh
@@ -9,6 +9,6 @@ ANSIBLE_ROLES_PATH="../" \
     ansible-playbook files/playbooks/vault_docker.yml "${@}"
 
 # copy generated integration_config.yml if it doesn't exist
-cp -n .output/integration_config.yml ../../ || true
+cp -n files/.output/integration_config.yml ../../ || true
 
 popd
--- tests/integration/targets/setup_localenv_gha/setup.sh
+++ tests/integration/targets/setup_localenv_gha/setup.sh
@@ -9,9 +9,9 @@ ANSIBLE_ROLES_PATH="../" \
     ansible-playbook files/playbooks/gha.yml "${@}"
 
 # launch containers
-.output/launch.sh
+files/.output/launch.sh
 
 # copy generated integration_config.yml if it doesn't exist
-cp -n .output/integration_config.yml ../../ || true
+cp -n files/.output/integration_config.yml ../../ || true
 
 popd
