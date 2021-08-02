547e3f536f45e84b44a98fb0729ce293094ab9a1
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-08-02 12:43:37 -0400

    tests - coverage, flags, and other testing/CI updates (#114)
    * try codecov upload at end (partial)
    
    * flags first try
    
    * add new action for ansible codecov
    
    * try separate script
    
    * cleanup
    
    * restore all tests
    
    * add tests/config.yml
    
    * special-case pre-2.12 units

---
+++ .github/actions/ansible-codecov/process.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2012, Brian Scholer (@briantist)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import sys
+import subprocess
+import re
+import getopt
+from pathlib import Path
+
+
+def get_flags(pattern, input):
+    patpat = r'\{([^\}]+)\}'
+
+    pats = re.findall(patpat, pattern)
+    matcher = re.sub(patpat, r'(.*?)', pattern)
+    match = re.search(matcher, input)
+
+    if match:
+        return [pats[i].replace('%', result) for i, result in enumerate(match.groups())]
+
+    return None
+
+
+def main(argv):
+    additional_flags = file_flag_pattern = directory_flag_pattern = directory = None
+
+    opts, args = getopt.getopt(argv, '', [
+        'directory=',
+        'directory-flag-pattern=',
+        'file-flag-pattern=',
+        'additional-flags=',
+    ])
+
+    for opt, arg in opts:
+        if opt == '--directory':
+            directory = arg
+        elif opt == '--directory-flag-pattern':
+            directory_flag_pattern = arg
+        elif opt == '--file-flag-pattern':
+            file_flag_pattern = arg
+        elif opt == '--additional-flags':
+            additional_flags = arg
+
+    extra_flags = additional_flags.split(',') if additional_flags else []
+
+    flags = {}
+
+    directory = Path(directory) if directory else Path.cwd()
+
+    for f in directory.rglob('*'):
+        if f.is_file():
+            iflags = set()
+            if directory_flag_pattern:
+                for part in f.parent.parts:
+                    dflags = get_flags(directory_flag_pattern, part)
+                    if dflags:
+                        iflags.update(dflags)
+
+            fflags = get_flags(file_flag_pattern, str(f.name))
+            if fflags:
+                iflags.update(fflags)
+
+            for flag in iflags:
+                flags.setdefault(flag, []).append(str(f.resolve()))
+
+    logextra = ' (+%r)' % extra_flags if extra_flags else ''
+
+    for flag, files in flags.items():
+        cmd = ['codecov', '-F', flag]
+        [cmd.extend(['-F', extra]) for extra in extra_flags]
+        [cmd.extend(['-f', file]) for file in files]
+
+        print('::group::Flag: %s%s' % (flag, logextra))
+
+        print('Executing: %r' % cmd)
+        subprocess.run(cmd, stderr=subprocess.STDOUT, check=True)
+
+        print('::endgroup::')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
