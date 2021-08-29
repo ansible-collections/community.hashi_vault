2d6eea74b140b0521dd44c48f02fec7c38557be2
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-08-28 20:16:05 -0400

    use ansible theme for docs (#133)
    * use ansible theme for docs
    
    * update files from newer sphinx-init

--- docs/preview/conf.py
+++ docs/preview/conf.py
@@ -3,7 +3,9 @@
 # http://www.sphinx-doc.org/en/master/config
 
 project = 'Ansible collections'
+copyright = 'Ansible contributors'
 
+title = 'Ansible Collections Documentation'
 html_short_title = 'Ansible Collections Documentation'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx_antsibull_ext']
@@ -12,7 +14,7 @@ pygments_style = 'ansible'
 
 highlight_language = 'YAML+Jinja'
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_ansible_theme'
 html_show_sphinx = False
 
 display_version = False
@@ -26,5 +28,9 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/2/', (None, '../python2.inv')),
     'python3': ('https://docs.python.org/3/', (None, '../python3.inv')),
     'jinja2': ('http://jinja.palletsprojects.com/', (None, '../jinja2.inv')),
-    'ansible_4': ('https://docs.ansible.com/ansible/4/', (None, '../ansible_4.inv')),
+    'ansible4': ('https://docs.ansible.com/ansible/4/', (None, '../ansible4.inv')),
 }
+
+default_role = 'any'
+
+nitpicky = True
