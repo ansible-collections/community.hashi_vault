a27b69c9a4a8d2d57fb2e3ee34994d49cf1714fb
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-10-11 14:27:15 -0400

    fix missing/misconfigured docstrings

--- plugins/doc_fragments/auth.py
+++ plugins/doc_fragments/auth.py
@@ -24,21 +24,26 @@ class ModuleDocFragment(object):
           - jwt
           - none
         default: token
+        type: str
       mount_point:
         description:
           - Vault mount point.
           - If not specified, the default mount point for a given auth method is used.
           - Does not apply to token authentication.
+        type: str
       token:
         description:
           - Vault token. Token may be specified explicitly, through the listed [env] vars, and also through the C(VAULT_TOKEN) env var.
           - If no token is supplied, explicitly or through env, then the plugin will check for a token file, as determined by I(token_path) and I(token_file).
           - The order of token loading (first found wins) is C(token param -> ansible var -> ANSIBLE_HASHI_VAULT_TOKEN -> VAULT_TOKEN -> token file).
+        type: str
       token_path:
         description: If no token is specified, will try to read the I(token_file) from this path.
+        type: str
       token_file:
         description: If no token is specified, will try to read the token from this file in I(token_path).
         default: '.vault-token'
+        type: str
       token_validate:
         description:
           - For token auth, will perform a C(lookup-self) operation to determine the token's validity before using it.
@@ -46,12 +51,44 @@ class ModuleDocFragment(object):
         type: boolean
         default: true
         version_added: 0.2.0
+      username:
+        description: Authentication user name.
+        type: str
+      password:
+        description: Authentication password.
+        type: str
       role_id:
         description: Vault Role ID. Used in approle and aws_iam_login auth methods.
+        type: str
       secret_id:
         description: Secret ID to be used for Vault AppRole authentication.
+        type: str
       jwt:
         description: The JSON Web Token (JWT) to use for JWT authentication to Vault.
+        type: str
+      aws_profile:
+        description: The AWS profile
+        type: str
+        aliases: [ boto_profile ]
+      aws_access_key:
+        description: The AWS access key to use.
+        type: str
+        aliases: [ aws_access_key_id ]
+      aws_secret_key:
+        description: The AWS secret key that corresponds to the access key.
+        type: str
+        aliases: [ aws_secret_access_key ]
+      aws_security_token:
+        description: The AWS security token if using temporary access and secret keys.
+        type: str
+      region:
+        description: The AWS region for which to create the connection.
+        type: str
+      aws_iam_server_id:
+        description: If specified, sets the value to use for the C(X-Vault-AWS-IAM-Server-ID) header as part of C(GetCallerIdentity) request.
+        required: False
+        type: str
+        version_added: '0.2.0'
     '''
 
     PLUGINS = r'''
@@ -67,8 +104,9 @@ class ModuleDocFragment(object):
           - name: ANSIBLE_HASHI_VAULT_AUTH_METHOD
             version_added: 0.2.0
         ini:
-          - section: lookup_hashi_vault
+          - section: hashi_vault_collection
             key: auth_method
+            version_added: 1.4.0
         vars:
           - name: ansible_hashi_vault_auth_method
             version_added: 1.2.0
@@ -90,8 +128,9 @@ class ModuleDocFragment(object):
           - name: ANSIBLE_HASHI_VAULT_TOKEN_PATH
             version_added: 0.2.0
         ini:
-          - section: lookup_hashi_vault
+          - section: hashi_vault_collection
             key: token_path
+            version_added: 1.4.0
         vars:
           - name: ansible_hashi_vault_token_path
             version_added: 1.2.0
@@ -106,8 +145,9 @@ class ModuleDocFragment(object):
           - name: ANSIBLE_HASHI_VAULT_TOKEN_FILE
             version_added: 0.2.0
         ini:
-          - section: lookup_hashi_vault
+          - section: hashi_vault_collection
             key: token_file
+            version_added: 1.4.0
         vars:
           - name: ansible_hashi_vault_token_file
             version_added: 1.2.0
@@ -115,11 +155,26 @@ class ModuleDocFragment(object):
         env:
           - name: ANSIBLE_HASHI_VAULT_TOKEN_VALIDATE
         ini:
-          - section: lookup_hashi_vault
+          - section: hashi_vault_collection
             key: token_validate
+            version_added: 1.4.0
         vars:
           - name: ansible_hashi_vault_token_validate
             version_added: 1.2.0
+      username:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_USERNAME
+            version_added: '1.2.0'
+        vars:
+          - name: ansible_hashi_vault_username
+            version_added: '1.2.0'
+      password:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_PASSWORD
+            version_added: '1.2.0'
+        vars:
+          - name: ansible_hashi_vault_password
+            version_added: '1.2.0'
       role_id:
         env:
           - name: VAULT_ROLE_ID
@@ -131,8 +186,9 @@ class ModuleDocFragment(object):
           - name: ANSIBLE_HASHI_VAULT_ROLE_ID
             version_added: 0.2.0
         ini:
-          - section: lookup_hashi_vault
+          - section: hashi_vault_collection
             key: role_id
+            version_added: 1.4.0
         vars:
           - name: ansible_hashi_vault_role_id
             version_added: 1.2.0
@@ -152,4 +208,34 @@ class ModuleDocFragment(object):
       jwt:
         env:
           - name: ANSIBLE_HASHI_VAULT_JWT
+      aws_profile:
+        env:
+          - name: AWS_DEFAULT_PROFILE
+          - name: AWS_PROFILE
+      aws_access_key:
+        env:
+          - name: EC2_ACCESS_KEY
+          - name: AWS_ACCESS_KEY
+          - name: AWS_ACCESS_KEY_ID
+      aws_secret_key:
+        env:
+          - name: EC2_SECRET_KEY
+          - name: AWS_SECRET_KEY
+          - name: AWS_SECRET_ACCESS_KEY
+      aws_security_token:
+        env:
+          - name: EC2_SECURITY_TOKEN
+          - name: AWS_SESSION_TOKEN
+          - name: AWS_SECURITY_TOKEN
+      region:
+        env:
+          - name: EC2_REGION
+          - name: AWS_REGION
+      aws_iam_server_id:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_AWS_IAM_SERVER_ID
+        ini:
+          - section: hashi_vault_collection
+            key: aws_iam_server_id
+            version_added: 1.4.0
     '''
