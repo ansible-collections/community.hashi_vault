b6cb19c107d906b36e73b10d4c7b95c6e709b928
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-11-04 21:06:49 -0400

    remove unused import

--- tests/unit/plugins/module_utils/authentication/test_hashi_vault_authenticator.py
+++ tests/unit/plugins/module_utils/authentication/test_hashi_vault_authenticator.py
@@ -5,7 +5,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import sys
 import pytest
 
 from ansible_collections.community.hashi_vault.tests.unit.compat import mock
