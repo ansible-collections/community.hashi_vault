5a3a24259f8de93664442a2d1566ba39d8a9a6b9
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-02-12 17:35:57 -0500

    more module unit coverage

--- tests/unit/plugins/modules/test_vault_token_create.py
+++ tests/unit/plugins/modules/test_vault_token_create.py
@@ -10,7 +10,6 @@ import pytest
 
 import json
 
-from ansible_collections.community.hashi_vault.tests.unit.compat import mock
 from .....plugins.modules import vault_token_create
 
 pytestmark = pytest.mark.usefixtures(
@@ -48,15 +47,11 @@ def _pass_thru_options():
     }
 
 
-def _combined_options():
+def _combined_options(**kwargs):
     opt = _connection_options()
     opt.update(_pass_thru_options())
+    opt.update(kwargs)
     return opt
-
-
-@pytest.fixture
-def connection_options():
-    return _connection_options()
 
 
 @pytest.fixture
@@ -80,8 +75,44 @@ def token_create_response(fixture_loader):
 
 class TestModuleVaultTokenCreate():
 
+    @pytest.mark.no_ansible_module_patch
+    def test_vault_token_create_passthru_options_expected(self, pass_thru_options):
+        # designed to catch the case where new passthru options differ between tests and module
+
+        module_set = set(vault_token_create.PASS_THRU_OPTION_NAMES)
+        test_set = set(pass_thru_options.keys())
+
+        assert sorted(vault_token_create.PASS_THRU_OPTION_NAMES) == sorted(pass_thru_options.keys()), (
+            "Passthru options in module do not match options in test: %r" % (
+                list(module_set ^ test_set)
+            )
+        )
+
+    @pytest.mark.no_ansible_module_patch
+    def test_vault_token_create_legacy_options_expected(self, legacy_option_translation, pass_thru_options):
+        # designed to catch the case where new legacy translations differ between tests and module
+        # and that all listed translations are present in passthru options
+
+        module_set = set(vault_token_create.LEGACY_OPTION_TRANSLATION.items())
+        test_set = set(legacy_option_translation.items())
+
+        module_key_set = set(vault_token_create.LEGACY_OPTION_TRANSLATION.keys())
+        pass_thru_key_set = set(pass_thru_options.keys())
+
+        assert module_set == test_set, (
+            "Legacy options in module do not match legacy options in test:\nmodule: %r\ntest: %r" % (
+                dict(module_set - test_set),
+                dict(test_set - module_set),
+            )
+        )
+        assert vault_token_create.LEGACY_OPTION_TRANSLATION.keys() <= pass_thru_options.keys(), (
+            "Legacy option translation keys must exist in passthru options: %r" % (
+                list(module_key_set - pass_thru_key_set),
+            )
+        )
+
     @pytest.mark.parametrize('patch_ansible_module', [_combined_options()], indirect=True)
-    def test_run(self, pass_thru_options, token_create_response, vault_client, capfd):
+    def test_vault_token_create_passthru_options(self, pass_thru_options, token_create_response, vault_client, capfd):
         client = vault_client
         client.auth.token.create.return_value = token_create_response
 
@@ -94,8 +125,76 @@ class TestModuleVaultTokenCreate():
         client.create_token.assert_not_called()
         client.auth.token.create.assert_called_once()
 
-        assert result['login'] == token_create_response
+        assert result['login'] == token_create_response, (
+            "module result did not match expected result:\nmodule: %r\nexpected: %r" % (result['login'], token_create_response)
+        )
 
+        if sys.version_info < (3, 8):
+            # TODO: remove when python < 3.8 is dropped
+            assert pass_thru_options.items() <= client.auth.token.create.call_args[1].items()
+        else:
+            assert pass_thru_options.items() <= client.auth.token.create.call_args.kwargs.items()
+
+    @pytest.mark.parametrize('patch_ansible_module', [_combined_options(orphan=True)], indirect=True)
+    def test_vault_token_create_legacy_options(self, pass_thru_options, legacy_option_translation, token_create_response, vault_client, capfd):
+        client = vault_client
+        client.create_token.return_value = token_create_response
+
+        with pytest.raises(SystemExit):
+            vault_token_create.main()
+
+        out, err = capfd.readouterr()
+        result = json.loads(out)
+
+        client.auth.token.create.assert_not_called()
+        client.create_token.assert_called_once()
+
+        assert result['login'] == token_create_response, (
+            "module result did not match expected result:\nmodule: %r\nexpected: %r" % (result['module'], token_create_response)
+        )
+
+        if sys.version_info < (3, 8):
+            # TODO: remove when python < 3.8 is dropped
+            call_kwargs = client.create_token.call_args[1]
+        else:
+            call_kwargs = client.create_token.call_args.kwargs
+
+        for name, legacy in legacy_option_translation.items():
+            assert name not in call_kwargs, (
+                "'%s' was found in call to legacy method, should be '%s'" % (name, legacy)
+            )
+            assert legacy in call_kwargs, (
+                "'%s' (from '%s') was not found in call to legacy method" % (legacy, name)
+            )
+            assert call_kwargs[legacy] == pass_thru_options.get(name), (
+                "Expected legacy param '%s' not found or value did not match:\nvalue: %r\nexpected: %r" % (
+                    legacy,
+                    call_kwargs.get(legacy),
+                    pass_thru_options.get(name),
+                )
+            )
+
+    @pytest.mark.parametrize('patch_ansible_module', [_combined_options(orphan=True)], indirect=True)
+    def test_vault_token_create_legacy_fallback(self, pass_thru_options, token_create_response, vault_client, module_warn, capfd):
+        client = vault_client
+        client.create_token.side_effect = AttributeError
+        client.auth.token.create.return_value = token_create_response
+
+        with pytest.raises(SystemExit):
+            vault_token_create.main()
+
+        out, err = capfd.readouterr()
+        result = json.loads(out)
+
+        module_warn.assert_called_once_with("'create_token' method was not found. Attempting method that requires root or sudo privileges.")
+        client.auth.token.create.assert_called_once()
+
+        assert result['login'] == token_create_response, (
+            "module result did not match expected result:\nmodule: %r\nexpected: %r" % (result['login'], token_create_response)
+        )
+
+        # we're retesting that expected options were passed, even though there's a separate test for that,
+        # to ensure that nothing in the original legacy attempt mutates the non-legacy options during fallback
         if sys.version_info < (3, 8):
             # TODO: remove when python < 3.8 is dropped
             assert pass_thru_options.items() <= client.auth.token.create.call_args[1].items()
