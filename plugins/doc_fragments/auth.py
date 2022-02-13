835233717c6e2d606e614bcf4fca675795aab796
chris93111 <christopheferreira@ymail.com>
2022-02-13 23:33:20 +0100

    add doc on params kubernetes_token_path

--- plugins/doc_fragments/auth.py
+++ plugins/doc_fragments/auth.py
@@ -76,6 +76,10 @@ class ModuleDocFragment(object):
       kubernetes_token:
         description: The Kubernetes Token (JWT) to use for Kubernetes authentication to Vault.
         type: str
+      kubernetes_token_path
+        description: If no kubernetes_token is specified, will try to read the token from this path.
+        default: '/var/run/secrets/kubernetes.io/serviceaccount/token'
+        type: str
       aws_profile:
         description: The AWS profile
         type: str
@@ -249,4 +253,22 @@ class ModuleDocFragment(object):
         ini:
           - section: hashi_vault_collection
             key: cert_auth_private_key
+      kubernetes_token:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_KUBERENTES_TOKEN
+            version_added: 0.2.3
+        vars:
+          - name: ansible_hashi_vault_kubernetes_token
+            version_added: 1.2.0
+      kubernetes_token_path:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_KUBERNETES_TOKEN_PATH
+            version_added: 0.2.3
+        ini:
+          - section: hashi_vault_collection
+            key: kubernetes_token_path
+            version_added: 2.3.0
+        vars:
+          - name: ansible_hashi_vault_kubernetes_token_path
+            version_added: 2.3.0
     '''
