f84b5b122737855880618cdd81f0c4db8c4043f8
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-03-04 14:28:55 -0500

    update docstring

--- plugins/lookup/vault_write.py
+++ plugins/lookup/vault_write.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = """
-  lookup: vault_write
+  name: vault_write
   version_added: 2.4.0
   author:
     - Brian Scholer (@briantist)
