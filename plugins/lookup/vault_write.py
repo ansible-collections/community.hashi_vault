3ebdb3fdda34a35ae22e70af04c6e6d84e8d9117
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-03-15 08:45:55 -0400

    typo

--- plugins/lookup/vault_write.py
+++ plugins/lookup/vault_write.py
@@ -25,7 +25,7 @@ DOCUMENTATION = """
   notes:
     - C(vault_write) is a generic plugin to do operations that do not yet have a dedicated plugin. Where a specific plugin exists, that should be used instead.
     - In the vast majority of cases, it will be better to do writes as a task, with the M(community.hashi_vault.vault_write) module.
-    - The lookup can be used in cases where you need a value directly in templating, but there is risk of executing the write many times uintentionally.
+    - The lookup can be used in cases where you need a value directly in templating, but there is risk of executing the write many times unintentionally.
     - The lookup is best used for endpoints that directly manipulate the input data and return a value, while not changing state in Vault.
     - See the R(Lookup Guide,ansible_collections.community.hashi_vault.docsite.lookup_guide) for more information.
   extends_documentation_fragment:
