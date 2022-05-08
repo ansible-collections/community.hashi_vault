cd44fc34088849782aa7844c998e1693b099b40a
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-05-08 00:53:30 -0400

    cleanup units

--- tests/unit/plugins/lookup/test_vault_ansible_settings.py
+++ tests/unit/plugins/lookup/test_vault_ansible_settings.py
@@ -7,12 +7,12 @@ __metaclass__ = type
 
 import pytest
 
+from ansible.plugins.loader import lookup_loader
 from ansible.plugins.lookup import LookupBase
 from ansible.errors import AnsibleError
 
 from ...compat import mock
 
-from .....plugins.lookup import vault_ansible_settings
 
 OPTIONS = {
     '_terms': (None, 'default'),
@@ -45,23 +45,8 @@ def patch_config_manager(sample_options):
         yield config
 
 
-# # if True, simulate 2.9 behavior on newer versions
-# @pytest.fixture(params=[False, True])
 @pytest.fixture
-def lookup_loader():
-    from ansible.plugins.loader import lookup_loader
-    loader = mock.Mock(wraps=lookup_loader)
-    return loader
-
-
-@pytest.fixture
-def patch_lookup_loader(lookup_loader):
-    with mock.patch('ansible.plugins.loader.lookup_loader', lookup_loader):
-        yield lookup_loader
-
-
-@pytest.fixture
-def vault_ansible_settings_lookup(lookup_loader):
+def vault_ansible_settings_lookup():
     return lookup_loader.get('community.hashi_vault.vault_ansible_settings')
 
 
@@ -142,92 +127,6 @@ class TestVaultAnsibleSettingsLookup(object):
             assert opt_inc_default or sample_options[k][1] != 'default'
             assert v == sample_options[k][0]
 
-
     def test_vault_ansible_settings_plugin_not_found(self, vault_ansible_settings_lookup):
         with pytest.raises(AnsibleError, match=r"'_ns._col._fake' plugin not found\."):
             vault_ansible_settings_lookup.run([], plugin='_ns._col._fake')
-
-    # @pytest.mark.parametrize('exc', [HashiVaultValueError('dummy msg'), NotImplementedError('dummy msg')])
-    # def test_vault_ansible_settings_authentication_error(self, vault_ansible_settings_lookup, minimal_vars, authenticator, exc):
-    #     authenticator.authenticate.side_effect = exc
-
-    #     with pytest.raises(AnsibleError, match=r'dummy msg'):
-    #         vault_ansible_settings_lookup.run(terms='fake', variables=minimal_vars)
-
-    # @pytest.mark.parametrize('exc', [HashiVaultValueError('dummy msg'), NotImplementedError('dummy msg')])
-    # def test_vault_ansible_settings_auth_validation_error(self, vault_ansible_settings_lookup, minimal_vars, authenticator, exc):
-    #     authenticator.validate.side_effect = exc
-
-    #     with pytest.raises(AnsibleError, match=r'dummy msg'):
-    #         vault_ansible_settings_lookup.run(terms='fake', variables=minimal_vars)
-
-    # @pytest.mark.parametrize('paths', [['fake1'], ['fake2', 'fake3']])
-    # @pytest.mark.parametrize('engine_mount_point', ['kv', 'other'])
-    # def test_vault_ansible_settings_return_data(self, vault_ansible_settings_lookup, minimal_vars, kv1_get_response, vault_client, paths, engine_mount_point):
-    #     client = vault_client
-
-    #     expected_calls = [mock.call(path=p, mount_point=engine_mount_point) for p in paths]
-
-    #     expected = {}
-    #     expected['raw'] = kv1_get_response.copy()
-    #     expected['metadata'] = kv1_get_response.copy()
-    #     expected['data'] = expected['metadata'].pop('data')
-    #     expected['secret'] = expected['data']
-
-    #     def _fake_kv1_get(path, mount_point):
-    #         r = kv1_get_response.copy()
-    #         r['data'] = r['data'].copy()
-    #         r['data'].update({'_path': path})
-    #         r['data'].update({'_mount': mount_point})
-    #         return r
-
-    #     client.secrets.kv.v1.read_secret = mock.Mock(wraps=_fake_kv1_get)
-
-    #     response = vault_ansible_settings_lookup.run(terms=paths, variables=minimal_vars, engine_mount_point=engine_mount_point)
-
-    #     client.secrets.kv.v1.read_secret.assert_has_calls(expected_calls)
-
-    #     assert len(response) == len(paths), "%i paths processed but got %i responses" % (len(paths), len(response))
-
-    #     for p in paths:
-    #         r = response.pop(0)
-    #         ins_p = r['secret'].pop('_path')
-    #         ins_m = r['secret'].pop('_mount')
-    #         assert p == ins_p, "expected '_path=%s' field was not found in response, got %r" % (p, ins_p)
-    #         assert engine_mount_point == ins_m, "expected '_mount=%s' field was not found in response, got %r" % (engine_mount_point, ins_m)
-    #         assert r['raw'] == expected['raw'], (
-    #             "remaining response did not match expected\nresponse: %r\nexpected: %r" % (r, expected['raw'])
-    #         )
-    #         assert r['metadata'] == expected['metadata']
-    #         assert r['data'] == expected['data']
-    #         assert r['secret'] == expected['secret']
-
-    # @pytest.mark.parametrize(
-    #     'exc',
-    #     [
-    #         (hvac.exceptions.Forbidden, "", r"^Forbidden: Permission Denied to path \['([^']+)'\]"),
-    #         (
-    #             hvac.exceptions.InvalidPath,
-    #             "Invalid path for a versioned K/V secrets engine",
-    #             r"^Invalid path for a versioned K/V secrets engine \['[^']+'\]. If this is a KV version 2 path, use community.hashi_vault.vault_kv2_get"
-    #         ),
-    #         (hvac.exceptions.InvalidPath, "", r"^Invalid or missing path \['[^']+'\]"),
-    #     ]
-    # )
-    # @pytest.mark.parametrize('path', ['path/1', 'second/path'])
-    # def test_vault_ansible_settings_exceptions(self, vault_ansible_settings_lookup, minimal_vars, vault_client, path, exc):
-    #     client = vault_client
-
-    #     client.secrets.kv.v1.read_secret.side_effect = exc[0](exc[1])
-
-    #     with pytest.raises(AnsibleError) as e:
-    #         vault_ansible_settings_lookup.run(terms=[path], variables=minimal_vars)
-
-    #     match = re.search(exc[2], str(e.value))
-
-    #     assert match is not None, "result: %r\ndid not match: %s" % (e.value, exc[2])
-
-    #     try:
-    #         assert path == match.group(1), "expected: %s\ngot: %s" % (match.group(1), path)
-    #     except IndexError:
-    #         pass
