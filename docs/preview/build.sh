34fb8f4d7f7f1351d667b2604cbb68cdb2c1985b
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-09-24 12:58:22 -0400

    update preview files

--- docs/preview/build.sh
+++ docs/preview/build.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 set -e
 pushd "${BASH_SOURCE%/*}"
 
 # Create collection documentation into temporary directory
 rm -rf temp-rst
 mkdir -p temp-rst
-antsibull-docs collection \
+antsibull-docs \
+    --config-file antsibull-docs.cfg \
+    collection \
     --use-current \
     --dest-dir temp-rst \
     community.hashi_vault
 
 # Copy collection documentation into source directory
-rsync -avc --delete-after temp-rst/collections/ rst/collections/
+rsync -cprv --delete-after temp-rst/collections/ rst/collections/
 
 # Build Sphinx site
-sphinx-build -M html rst build -c .
+sphinx-build -M html rst build -c . -W --keep-going
 
 popd
--- docs/preview/rst/index.rst
+++ docs/preview/rst/index.rst
@@ -1,9 +1,13 @@
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 .. _docsite_root_index:
 
 Ansible collection documentation preview
 ========================================
 
-This docsite contains documentation from ``community.hashi_vault``.
+This docsite contains documentation for ``community.hashi_vault``.
 
 
 .. toctree::
