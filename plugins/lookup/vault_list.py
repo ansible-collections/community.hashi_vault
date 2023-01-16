3d981bba008d23e7d9698a80511b364e6bdfc75d
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-01-16 14:09:38 -0500

    more list lookup examples and formatting

--- plugins/lookup/vault_list.py
+++ plugins/lookup/vault_list.py
@@ -33,7 +33,7 @@ DOCUMENTATION = """
 EXAMPLES = """
 - name: List all secrets at a path
   ansible.builtin.debug:
-    msg: "{{ lookup('community.hashi_vault.vault_list', 'secret', url='https://vault:8201') }}"
+    msg: "{{ lookup('community.hashi_vault.vault_list', 'secret/metadata/path', url='https://vault:8201') }}"
 
 - name: List access policies
   ansible.builtin.debug:
@@ -42,7 +42,7 @@ EXAMPLES = """
 - name: Perform multiple list operations with a single Vault login
   vars:
     paths:
-      - secret
+      - secret/metadata/path
       - sys/policies/acl
   ansible.builtin.debug:
     msg: "{{ lookup('community.hashi_vault.vault_list', *paths, auth_method='userpass', username=user, password=pwd) }}"
@@ -50,7 +50,7 @@ EXAMPLES = """
 - name: Perform multiple list operations with a single Vault login in a loop
   vars:
     paths:
-      - secret
+      - secret/metadata/path
       - sys/policies/acl
   ansible.builtin.debug:
     msg: '{{ item }}'
@@ -64,22 +64,58 @@ EXAMPLES = """
   ansible.builtin.debug:
     msg: '{{ item }}'
   with_community.hashi_vault.vault_list:
-    - secret
+    - secret/metadata/path
     - sys/policies/acl
 
-- name: Create dictionary fact consisting of secret name (e.g. username) and value of a key (e.g. 'password') within that secret
+- name: Create fact consisting of list of dictionaries each with secret name (e.g. username) and value of a key (e.g. 'password') within that secret
   ansible.builtin.set_fact:
-    credentials: "{{ credentials|default([]) + [ {'username': item, 'password': lookup('community.hashi_vault.vault_kv2_get', item, engine_mount_point='vpn-users').secret.password} ] }}"
+    credentials: >-
+      {{
+        credentials
+        | default([]) + [
+          {
+            'username': item,
+            'password': lookup('community.hashi_vault.vault_kv2_get', item, engine_mount_point='vpn-users').secret.password
+          }
+        ]
+      }}
   loop: "{{ query('community.hashi_vault.vault_list', 'vpn-users/metadata')[0].data['keys'] }}"
   no_log: true
+
 - ansible.builtin.debug:
     msg: "{{ credentials }}"
+
+- name: Create the same as above without looping, and only 2 logins
+  vars:
+    secret_names: >-
+      {{
+        query('community.hashi_vault.vault_list', 'vpn-users/metadata')
+        | map(attribute='data')
+        | map(attribute='keys')
+        | flatten
+      }}
+    secret_values: >-
+      {{
+        lookup('community.hashi_vault.vault_kv2_get', *secret_names, engine_mount_point='vpn-users')
+        | map(attribute='secret')
+        | map(attribute='password')
+        | flatten
+      }}
+    credentials_dict: "{{ dict(secret_names | zip(secret_values)) }}"
+  ansible.builtin.set_fact:
+    credentials_dict: "{{ credentials_dict }}"
+    credentials_list: "{{ credentials_dict | dict2items(key_name='username', value_name='password') }}"
+  no_log: true
+
+- ansible.builtin.debug:
+    msg:
+      - "Dictionary: {{ credentials_dict }}"
+      - "List: {{ credentials_list }}"
 
 - name: List all userpass users and output the token policies for each user
   ansible.builtin.debug:
     msg: "{{ lookup('community.hashi_vault.vault_read', 'auth/userpass/users/' + item).data.token_policies }}"
   loop: "{{ query('community.hashi_vault.vault_list', 'auth/userpass/users')[0].data['keys'] }}"
-
 """
 
 RETURN = """
