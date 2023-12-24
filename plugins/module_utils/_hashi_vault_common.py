0d4f68446e534f4d784fe772d4bd2b8856f109f0
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-12-24 12:15:47 -0500

    stop stringifying with newer requests

--- plugins/module_utils/_hashi_vault_common.py
+++ plugins/module_utils/_hashi_vault_common.py
@@ -24,83 +24,19 @@ try:
 except ImportError:
     HAS_HVAC = False
 
-# try:
-#     from ansible.utils.unsafe_proxy import AnsibleUnsafe, AnsibleUnsafeBytes
-# except ImportError:
-#     class AnsibleUnsafe:
-#         pass
-
-#     class AnsibleUnsafeBytes(bytes, AnsibleUnsafe):
-#         pass
-
-
-def _stringify(input):
-    '''
-    This method is primarily used to Un-Unsafe values that come from Ansible.
-    We want to remove the Unsafe context so that libraries don't get confused
-    by the values.
-    '''
-
-    # if not isinstance(input, AnsibleUnsafe):
-    #     return input
-
-    if hasattr(input, "_strip_unsafe"):
-        # if isinstance(input, AnsibleUnsafeBytes):
-        #     # return input._strip_unsafe()
-        #     # raise
-        #     return input.__bytes__()
-        # else:
-        try:
-            return input._strip_unsafe()
-        except AttributeError:
-            return input.__bytes__()
-    # except AttributeError:
-    #     raise
-    #     # This version of Ansible doesn't contain the method.
-    #     # Use the old way.
-    #     pass
-
-    # Since this is a module_util, and will be used by both plugins and modules,
-    # we cannot import the AnsibleUnsafe* types, because they are controller-only.
-    # However, they subclass the native types, so we can check for that.
-
-    # bytes is the only consistent type to check against in both py2 and py3
-    if isinstance(input, bytes):
-        # seems redundant, but this will give us a regular bytes object even
-        # when the input is AnsibleUnsafeBytes (in older Ansible versions)
-        return bytes(input)
-    else:
-        # instead of checking for py2 vs. py3 to cast to str or unicode,
-        # let's get the type from the literal.
-        return type(u'')(input)
-
 
 class HashiVaultValueError(ValueError):
     '''Use in common code to raise an Exception that can be turned into AnsibleError or used to fail_json()'''
 
 
 class HashiVaultHelper():
-
-    STRINGIFY_CANDIDATES = set([
-        'token',    # Token will end up in a header, requests requires headers to be str or bytes,
-                    # and newer versions of requests stopped converting automatically. Because our
-                    # token could have been passed in from a previous lookup call, it could be one
-                    # of the AnsibleUnsafe types instead, causing a failure. Tokens should always
-                    # be strings, so we will convert them.
-        'namespace',    # namespace is also set in a header
-    ])
-
     def __init__(self):
         # TODO move hvac checking here?
         pass
 
-    @staticmethod
-    def _stringify(input):
-        return _stringify(input)
-
     def get_vault_client(
         self,
-        hashi_vault_logout_inferred_token=True, hashi_vault_revoke_on_logout=False, hashi_vault_stringify_args=True,
+        hashi_vault_logout_inferred_token=True, hashi_vault_revoke_on_logout=False,
         **kwargs
     ):
         '''
@@ -112,15 +48,7 @@ class HashiVaultHelper():
 
         :param hashi_vault_revoke_on_logout: if True revokes any current token on logout. Only used if a logout is performed. Not recommended.
         :type hashi_vault_revoke_on_logout: bool
-
-        :param hashi_vault_stringify_args: if True converts a specific set of defined kwargs to a string type.
-        :type hashi_vault_stringify_args: bool
         '''
-
-        if hashi_vault_stringify_args:
-            for key in kwargs.keys():
-                if key in self.STRINGIFY_CANDIDATES:
-                    kwargs[key] = self._stringify(kwargs[key])
 
         client = hvac.Client(**kwargs)
 
@@ -324,7 +252,3 @@ class HashiVaultAuthMethodBase(HashiVaultOptionGroupBase):
 
     def deprecate(self, message, version=None, date=None, collection_name=None):
         self._deprecator(message, version=version, date=date, collection_name=collection_name)
-
-    @staticmethod
-    def _stringify(input):
-        return _stringify(input)
