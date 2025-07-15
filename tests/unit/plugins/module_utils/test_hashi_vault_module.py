9c8e8aee021f55f2fb5f66c4b3a88e2abc8a69e1
Brian Scholer <1260690+briantist@users.noreply.github.com>
2025-07-15 09:37:45 -0400

    Tests/fix various issues 2.19 (#467)
    * fix templating in approle_test_controller integration test
    
    * update to tests to reflect Vault db engine behavior change
    
    * update unit test module argument patching for 2.19
    
    * fix options adapter units for 2.19
    
    * fix vault_pki_generate_certificate  units for 2.19
    
    * remove unused import
    
    * check module failure for older ansible versions too
    
    * coverage for set_module_args conditionals
    
    * simplify sanity testing

--- tests/unit/plugins/module_utils/test_hashi_vault_module.py
+++ tests/unit/plugins/module_utils/test_hashi_vault_module.py
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import pytest
-from json import dumps
-from ansible.module_utils import basic
+import json
+import contextlib
 from ansible.module_utils.common.text.converters import to_bytes
 
 from ...compat import mock
@@ -30,46 +30,79 @@ def fail_json(*args, **kwargs):
     raise AnsibleFailJson(kwargs)
 
 
-class TestHashiVaultModule:
+@contextlib.contextmanager
+def set_module_args(args):
+    """
+    Context manager that sets module arguments for AnsibleModule
+    """
+    if '_ansible_remote_tmp' not in args:
+        args['_ansible_remote_tmp'] = '/tmp'
+    if '_ansible_keep_remote_files' not in args:
+        args['_ansible_keep_remote_files'] = False
 
-    def test_init_success(self, generate_argspec):
+    try:
+        from ansible.module_utils.testing import patch_module_args
+    except ImportError:
+        # Before data tagging support was merged, this was the way to go:
+        from ansible.module_utils import basic
+        serialized_args = to_bytes(json.dumps({'ANSIBLE_MODULE_ARGS': args}))
+        with mock.patch.object(basic, '_ANSIBLE_ARGS', serialized_args):
+            yield
+    else:
+        # With data tagging support, we have a new helper for this:
+        with patch_module_args(args):
+            yield
+
+
+MODULE_ARGS_LIST = [
+    {},
+    {
+        '_ansible_remote_tmp': '/tmp',
+        '_ansible_keep_remote_files': True,
+    },
+]
+
+
+class TestHashiVaultModule:
+    @pytest.mark.parametrize('module_args', MODULE_ARGS_LIST)
+    def test_init_success(self, generate_argspec, module_args):
         """Test successful initialization of HashiVaultModule."""
         with mock.patch('ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_module.AnsibleModule') as mock_ansible_module:
             with mock.patch('ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_module.HashiVaultHelper') as mock_helper:
-                basic._ANSIBLE_ARGS = to_bytes(dumps({'ANSIBLE_MODULE_ARGS': {}}))
-                module = HashiVaultModule(argument_spec=generate_argspec)
+                with set_module_args(module_args):
+                    module = HashiVaultModule(argument_spec=generate_argspec)
 
-                # Check if HashiVaultHelper was initialized
-                mock_helper.assert_called_once()
+                    # Check if HashiVaultHelper was initialized
+                    mock_helper.assert_called_once()
 
-                # Ensure that the helper, adapter, connection options, and authenticator are set correctly
-                assert module.helper is not None
-                assert module.adapter is not None
-                assert module.connection_options is not None
-                assert module.authenticator is not None
+                    # Ensure that the helper, adapter, connection options, and authenticator are set correctly
+                    assert module.helper is not None
+                    assert module.adapter is not None
+                    assert module.connection_options is not None
+                    assert module.authenticator is not None
 
-    def test_init_hvac_error(self, generate_argspec):
+    @pytest.mark.parametrize('module_args', MODULE_ARGS_LIST)
+    def test_init_hvac_error(self, generate_argspec, module_args):
         """Test that HashiVaultHVACError triggers a call to fail_json."""
         with mock.patch('ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_module.HashiVaultModule.fail_json',
                         wraps=fail_json) as mock_ansible_fail:
 
             with mock.patch('ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault_module.HashiVaultHelper',
                             side_effect=HashiVaultHVACError("Error occurred", "error_trace")) as mock_helper:
-                basic._ANSIBLE_ARGS = to_bytes(dumps({'ANSIBLE_MODULE_ARGS': {}}))
+                with set_module_args(module_args):
+                    with pytest.raises(AnsibleFailJson) as exc_info:
+                        # Initialize the module, which should trigger fail_json
+                        module = HashiVaultModule(argument_spec=generate_argspec)
 
-                with pytest.raises(AnsibleFailJson) as exc_info:
-                    # Initialize the module, which should trigger fail_json
-                    module = HashiVaultModule(argument_spec=generate_argspec)
+                    # Optionally, assert the failure message and exception
+                    assert exc_info.value.args[0]['failed'] is True
+                    assert exc_info.value.args[0]['msg'] == "error_trace"
+                    assert exc_info.value.args[0]['exception'] == "Error occurred"
 
-                # Optionally, assert the failure message and exception
-                assert exc_info.value.args[0]['failed'] is True
-                assert exc_info.value.args[0]['msg'] == "error_trace"
-                assert exc_info.value.args[0]['exception'] == "Error occurred"
-
-                mock_ansible_fail.assert_called_once_with(
-                    msg="error_trace",
-                    exception="Error occurred"
-                )
+                    mock_ansible_fail.assert_called_once_with(
+                        msg="error_trace",
+                        exception="Error occurred"
+                    )
 
     def test_generate_argspec(self):
         """Test that generate_argspec correctly combines connection, authentication, and extra arguments."""
